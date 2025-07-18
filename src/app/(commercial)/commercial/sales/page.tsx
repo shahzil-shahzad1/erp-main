c4fb7c70139dfbdb455ebb6c6995ef0200aabfba
Shahzil Shahzad <silentboys1117@gmail.com>
2025-07-18 17:36:49 +0500

    Update page.tsx

--- src/app/(commercial)/commercial/sales/page.tsx
+++ src/app/(commercial)/commercial/sales/page.tsx
@@ -1,15 +1,7 @@
-"use client"
-
-
-import CommercialSales from "@/components/commercial/CommercialSales"
-
-
 export default function CommercialSalesPage() {
       return (
-    <div className="h-full w-full overflow-auto">
-      <div className="min-h-full w-full max-w-none">
-        <CommercialSales />
-      </div>
+     <div>
+      Hello invoices
     </div>
   )
 }
